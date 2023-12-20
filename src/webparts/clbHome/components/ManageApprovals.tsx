ca2ee9969ff481b1f2ab679573023af9385638a1
Priyadharshini Gunasekaran <v-prigunasek@microsoft.com>
2023-12-20 13:42:19 +0530

    2.6 release changes

--- src/webparts/clbHome/components/ManageApprovals.tsx
+++ src/webparts/clbHome/components/ManageApprovals.tsx
@@ -1,4 +1,4 @@
-import { IPivotItemProps, Pivot, PivotItem, PivotLinkFormat } from '@fluentui/react';
+import { IPivotItemProps, Pivot, PivotItem } from '@fluentui/react';
 import { WebPartContext } from "@microsoft/sp-webpart-base";
 import * as LocaleStrings from 'ClbHomeWebPartStrings';
 import React, { Component } from 'react';
@@ -7,6 +7,7 @@ import styles from "../scss/ManageApprovals.module.scss";
 import ApproveChampion from './ApproveChampion';
 import ChampionsActivities from './ChampionsActivities';
 import ManageConfigSettings from './ManageConfigSettings';
+import * as stringsConstants from "../constants/strings";
 
 //declaring common services object
 let commonServiceManager: commonServices;
@@ -16,19 +17,43 @@ export interface IManageApprovalsProps {
     onClickBack: Function;
     isPendingChampionApproval: boolean;
     isPendingEventApproval: boolean;
+    appTitle: string;
+    updateAppTitle: Function;
+    currentThemeName?: string;
 }
-export interface IManageApprovalsState { }
+export interface IManageApprovalsState {
+    isPendingChampionApproval: boolean;
+    isPendingEventApproval: boolean;
+    appTitle: string;
+}
 export default class ManageApprovals extends Component<IManageApprovalsProps, IManageApprovalsState> {
 
     constructor(props: IManageApprovalsProps) {
         super(props);
-        this.state = {};
+        this.state = {
+            isPendingChampionApproval: this.props.isPendingChampionApproval,
+            isPendingEventApproval: this.props.isPendingEventApproval,
+            appTitle: this.props.appTitle
+        };
+
+        this.setState = this.setState.bind(this);
 
         //Create object for CommonServices class
         commonServiceManager = new commonServices(
             this.props.context,
             this.props.siteUrl
         );
+    }
+
+
+    //Updating the state of parent component whenever this component is updated 
+    public componentDidUpdate(prevProps: Readonly<IManageApprovalsProps>, prevState: Readonly<IManageApprovalsState>, snapshot?: any): void {
+        //updating state of the parent component 'ClbHome" to show the new app title in app header and breadcrumb
+        if (prevState.appTitle !== this.state.appTitle) {
+            this.props.updateAppTitle({
+                appTitle: this.state.appTitle
+            });
+        }
     }
 
     _customRenderer(
@@ -64,59 +89,76 @@ export default class ManageApprovals extends Component<IManageApprovalsProps, IM
 
 
     public render() {
+        const isDarkOrContrastTheme = this.props.currentThemeName === stringsConstants.themeDarkMode || this.props.currentThemeName === stringsConstants.themeContrastMode;
         return (
-            <div className={`container ${styles.manageApprovalsContainer}`}>
+            <div className={`container ${styles.manageApprovalsContainer}${isDarkOrContrastTheme ? " " + styles.manageApprovalsContainerDarkContrast : ""}`}>
                 <div className={styles.manageApprovalsPath}>
                     <img src={require("../assets/CMPImages/BackIcon.png")}
                         className={styles.backImg}
                         alt={LocaleStrings.BackButton}
+                        aria-hidden="true"
                     />
                     <span
                         className={styles.backLabel}
                         onClick={() => { this.props.onClickBack(); }}
-                        title={LocaleStrings.CMPBreadcrumbLabel}
+                        role="button"
+                        tabIndex={0}
+                        onKeyDown={(evt: any) => { if (evt.key === stringsConstants.stringEnter) this.props.onClickBack(); }}
+                        aria-label={this.props.appTitle}
                     >
-                        {LocaleStrings.CMPBreadcrumbLabel}
+                        <span title={this.props.appTitle}>
+                            {this.props.appTitle}
+                        </span>
                     </span>
                     <span className={styles.border}></span>
                     <span className={styles.manageApprovalsLabel}>{LocaleStrings.AdminTasksLabel}</span>
                 </div>
                 <Pivot
-                    linkFormat={PivotLinkFormat.tabs}
+                    linkFormat='tabs'
                     className={styles.manageApprovalsPivot}
                     defaultSelectedKey="0"
                 >
                     <PivotItem
                         headerText={LocaleStrings.ChampionsListPageTitle}
                         itemKey="0"
-                        headerButtonProps={{ title: LocaleStrings.ChampionsListPageTitle }}
-                        onRenderItemLink={this.props.isPendingChampionApproval ? this._customRenderer : this._customRendererNoIcon}
+                        onRenderItemLink={this.state.isPendingChampionApproval ? this._customRenderer : this._customRendererNoIcon}
+                        ariaLabel={navigator.userAgent.match(/Android/i) ? `${LocaleStrings.ChampionsListPageTitle} 1 of 3` : ""}
                     >
-                        <ApproveChampion
-                            context={this.props.context}
-                            siteUrl={this.props.siteUrl}
-                        />
+                        <span title={LocaleStrings.ChampionsListPageTitle}>
+                            <ApproveChampion
+                                context={this.props.context}
+                                siteUrl={this.props.siteUrl}
+                                setState={this.setState}
+                            />
+                        </span>
                     </PivotItem>
                     <PivotItem
                         headerText={LocaleStrings.ChampionActivitiesLabel}
                         itemKey="1"
-                        headerButtonProps={{ title: LocaleStrings.ChampionActivitiesLabel }}
-                        onRenderItemLink={this.props.isPendingEventApproval ? this._customRenderer : this._customRendererNoIcon}
+                        onRenderItemLink={this.state.isPendingEventApproval ? this._customRenderer : this._customRendererNoIcon}
+                        ariaLabel={navigator.userAgent.match(/Android/i) ? `${LocaleStrings.ChampionActivitiesLabel} 2 of 3` : ""}
                     >
-                        <ChampionsActivities
-                            context={this.props.context}
-                            siteUrl={this.props.siteUrl}
-                        />
+                        <span title={LocaleStrings.ChampionActivitiesLabel}>
+                            <ChampionsActivities
+                                context={this.props.context}
+                                siteUrl={this.props.siteUrl}
+                                setState={this.setState}
+                            />
+                        </span>
                     </PivotItem>
                     <PivotItem
                         headerText={LocaleStrings.ManageConfigSettingsLabel}
                         itemKey="2"
-                        headerButtonProps={{ title: LocaleStrings.ManageConfigSettingsLabel }}
+                        ariaLabel={navigator.userAgent.match(/Android/i) ? `${LocaleStrings.ManageConfigSettingsLabel} 3 of 3` : ""}
                     >
-                        <ManageConfigSettings
-                            context={this.props.context}
-                            siteUrl={this.props.siteUrl}
-                        />
+                        <span title={LocaleStrings.ManageConfigSettingsLabel}>
+                            <ManageConfigSettings
+                                context={this.props.context}
+                                siteUrl={this.props.siteUrl}
+                                appTitle={this.props.appTitle}
+                                updateAppTitle={this.setState}
+                            />
+                        </span>
                     </PivotItem>
                 </Pivot>
             </div>
